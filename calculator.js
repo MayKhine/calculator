958c6d74f7354a20b8a079085c5236af120c3ff9
May <maykhine.ada@gmail.com>
2022-11-08 22:27:54 -0500

    update the calculator function and css style

--- calculator.js
+++ calculator.js
@@ -1,6 +1,5 @@
 const operate = (operator, num1, num2) => {
     let result = 0;
-    console.log({ operator, num1, num2 })
     switch (operator) {
         case '+':
             result = (num1 + num2);
@@ -15,14 +14,29 @@ const operate = (operator, num1, num2) => {
             result = (num1 / num2);
             break
     }
-    return parseFloat(result.toFixed(3));
+    return result; //parseFloat(result.toFixed(3));
 }
+let calcFlag = 0;
 const operateAndReset = () => {
+
     calculatedVal = operate(mathEquation[1], parseFloat(mathEquation[0]), parseFloat(mathEquation[2]))
-    console.log({ calculatedVal })
+    // tempMathEquation = mathEquation.push('=')
+    // mathEquation.push('=');
     mathEquation = [];
     mathEquation.push(calculatedVal);
+
+    // console.log({ calculatedVal })
+    // euqationReset = 1;
     prevVal = '';
+    calculatedVal = 0;
+    calcFlag = 1;
+}
+
+const operateForEqual = () => {
+    calculatedVal = operate(mathEquation[1], parseFloat(mathEquation[0]), parseFloat(mathEquation[2]))
+    mathEquation.push('=');
+    mathEquation.push(calculatedVal);
+    prevVal = calculatedVal;
     calculatedVal = 0;
 }
 const percentage = (num) => {
@@ -33,10 +47,6 @@ const flipVal = (num) => {
     return num * (-1)
 }
 
-const checkEquation = (mathArr) => {
-
-}
-
 const isOperator = (value) => {
     if (value == '+' || value == '-' || value == '*' || value == '/') {
         return true
@@ -44,20 +54,76 @@ const isOperator = (value) => {
         return false
     }
 }
-// const result = operate('*', 3.123, 3)
-// const result = flipVal(-81)
-// console.log('Result: ', result)
+
+const display = (arr) => {
+    displayScreen.forEach((i) => {
+        if (prevVal == '' || prevVal == 'REPLACE') {
+            if (arr.length > 0 && isOperator(arr[arr.length - 1]) != true) {
+                // i.innerHTML = arr[arr.length - 1]
+                i.innerHTML = roundDeci(arr[arr.length - 1])
+
+            }
+            else {
+                if (arr.length >= 2) {
+                    // i.innerHTML = arr[arr.length - 2]
+                    i.innerHTML = roundDeci(arr[arr.length - 2])
+
+                }
+                else {
+                    i.innerHTML = 0
+                }
+            }
+        } else {
+            // i.innerHTML = prevVal
+            i.innerHTML = roundDeci(prevVal)
+
+        }
+    })
+}
+
+const roundDeci = (num) => {
+    // return parseFloat(num.toFixed(10));
+    num = num.toString()
+
+    let deciPosition = num.indexOf('.')
+    let ePosition = num.indexOf('e')
+
+    if (calcFlag == 1) {
+        if (deciPosition != -1 && (ePosition - deciPosition) > 5) {
+            num = num.substr(0, (deciPosition + 6)) + num.substr(ePosition)
+
+        } else if (deciPosition != -1 && (num.length - deciPosition) > 5) {
+            num = parseFloat(num).toFixed(5)
+        }
+        calcFlag = 0
+        mathEquation.pop()
+        mathEquation.push(num)
+    }
+    return num
+}
 
 let mathEquation = [];
+let euqationReset = 0;
 let prevVal = '';
 let calculatedVal = 0;
 const containerEle = document.querySelector(".container");
 const gridContainerEle = document.querySelector(".grid-container");
 let gridItems = document.querySelectorAll('.grid-item')
+// let displayScreen = document.querySelectorAll('.display')
+
+let displayScreen = document.querySelectorAll('.calc-display')
+// let displayScreen2 = document.querySelectorAll('.calc-display2')
+
 let deciFlag = 0;
 
 gridItems.forEach((item) => {
     item.addEventListener('click', (item) => {
+        // if (euqationReset == 1) {
+        //     let tempVal = mathEquation[mathEquation.length - 2]
+        //     mathEquation = []
+        //     mathEquation.push(tempVal)
+        //     euqationReset = 0;
+        // }
         let itemVal = item.target.getAttribute('value')
 
         // AC is clicked, clear everything
@@ -65,20 +131,19 @@ gridItems.forEach((item) => {
             mathEquation = [];
             prevVal = '';
             deciFlag = 0;
-            console.log('AC : ', { mathEquation, prevVal })
+            // console.log('AC : ', { mathEquation, prevVal })
         }
-
         // = is clicked
         else if (itemVal == 'equal') {
-            console.log('CALL THE OPERATE FUNCTION')
             // call the operate function
             if (mathEquation.length > 2) {
+
                 operateAndReset();
+                // operateForEqual();
             }
         }
-
         // +/- is clicked, flip the last input number
-        else if (itemVal == 'flip' && mathEquation.length > 0) {
+        else if (itemVal == 'flip' && mathEquation.length > 0 && prevVal != '') {
             let lastVal = mathEquation[mathEquation.length - 1]
 
             if (isOperator(lastVal)) {
@@ -93,7 +158,7 @@ gridItems.forEach((item) => {
             deciFlag = 0;
         }
         //percent is clicked
-        else if (itemVal == 'percent' && mathEquation.length > 0) {
+        else if (itemVal == 'percent' && mathEquation.length > 0 && prevVal != '') {
             const lastVal = mathEquation[mathEquation.length - 1]
             mathEquation.pop()
             mathEquation.push(percentage(lastVal))
@@ -104,9 +169,12 @@ gridItems.forEach((item) => {
         else if (itemVal == 'deci') {
             if (deciFlag == 0) {
                 if (prevVal) {
+                    mathEquation.pop()
                     prevVal = prevVal + '.'
+                    mathEquation.push(prevVal)
                 } else {
                     prevVal = '0' + '.'
+                    mathEquation.push(prevVal)
                 }
                 deciFlag = 1
             }
@@ -120,6 +188,7 @@ gridItems.forEach((item) => {
                 if (prevVal == 'REPLACE') {
                     itemVal = itemVal
                 }
+
                 else {
                     itemVal = prevVal + '' + itemVal
                 }
@@ -127,13 +196,16 @@ gridItems.forEach((item) => {
                 prevVal = itemVal
             }
             else {
+                if (!isOperator(mathEquation[mathEquation.length - 1])) {
+                    //reset and add the new 
+                    mathEquation = []
+                }
                 mathEquation.push(itemVal)
                 prevVal = itemVal
             }
         }  //if the math equation length is longer than 2 then calcualte
         else if (mathEquation.length > 2) {
-
-            console.log('CALL THE OPERATE FUNCTION')
+            // console.log('CALL THE OPERATE FUNCTION')
             operateAndReset();
             mathEquation.push(itemVal)
         }
@@ -149,5 +221,6 @@ gridItems.forEach((item) => {
         }
 
         console.log(mathEquation)
+        display(mathEquation)
     })
 })
